e57e0ef5fadbad1c0eade70a257302b82dea1a81
Jie <1007172767@qq.com>
2017-05-21 14:37:23 +0800

    net

---
+++ learn/api_net.js
@@ -0,0 +1,40 @@
+/*
+不严谨的服务端
+*/
+
+net.createServer(function(conn){
+	conn.on('data',function(data){
+		conn.write([
+			'HTTP/1.1 200 OK',
+            'Content-Type: text/plain',
+            'Content-Length: 11',
+            '',
+            'Hello World'
+			].join(''));
+	});
+}).listen(80);
+
+/*
+客户端
+*/
+
+var options = {
+        port: 80,
+        host: 'www.example.com'
+    };
+
+var client = net.connect(options, function () {
+        client.write([
+            'GET / HTTP/1.1',
+            'User-Agent: curl/7.26.0',
+            'Host: www.baidu.com',
+            'Accept: */*',
+            '',
+            ''
+        ].join(''));
+    });
+
+client.on('data', function (data) {
+    console.log(data.toString());
+    client.end();
+});
