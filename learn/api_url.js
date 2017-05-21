0b2cdc6537ac6d6af8da63676abf5bfb67472b08
Jie <1007172767@qq.com>
2017-05-21 14:28:09 +0800

    url

---
+++ learn/api_url.js
@@ -0,0 +1,51 @@
+url.parse('http://user:pass@host.com:8080/p/a/t/h?query=string#hash');
+/* =>
+{ protocol: 'http:',
+  auth: 'user:pass',
+  host: 'host.com:8080',
+  port: '8080',
+  hostname: 'host.com',
+  hash: '#hash',
+  search: '?query=string',
+  query: 'query=string',
+  pathname: '/p/a/t/h',
+  path: '/p/a/t/h?query=string',
+  href: 'http://user:pass@host.com:8080/p/a/t/h?query=string#hash' }
+*/
+
+
+http.createServer(function (request, response) {
+    var tmp = request.url; // => "/foo/bar?a=b"
+    url.parse(tmp);
+    /* =>
+    { protocol: null,
+      slashes: null,
+      auth: null,
+      host: null,
+      port: null,
+      hostname: null,
+      hash: null,
+      search: '?a=b',
+      query: 'a=b',
+      pathname: '/foo/bar',
+      path: '/foo/bar?a=b',
+      href: '/foo/bar?a=b' }
+    */
+}).listen(80);
+
+
+url.format({
+    protocol: 'http:',
+    host: 'www.example.com',
+    pathname: '/p/a/t/h',
+    search: 'query=string'
+});
+/* =>
+'http://www.example.com/p/a/t/h?query=string'
+*/
+
+
+url.resolve('http://www.example.com/foo/bar', '../baz');
+/* =>
+http://www.example.com/baz
+*/
