5a4b84eb796e3d5f0ca5348864ca59484bd35a85
Jie <1007172767@qq.com>
2017-06-05 22:15:24 +0800

    www

--- try.js
+++ try.js
@@ -1,5 +1,6 @@
-console.log(1);
-process.nextTick(function(){
-	console.log(3);
-})
-console.log(2);
+process.stdin.resume();
+process.stdin.on("data",function(chunk){
+	var buf = Array.prototype.slice.call(chunk.toString("utf8"));
+	console.log(buf.slice(0,buf.length-2).join(''));
+	process.stdout.write(chunk);
+});
